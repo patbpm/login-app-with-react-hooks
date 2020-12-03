2599910247c3313135871ac713bd7d861a422ad7
Patrick <patbpm1@gmail.com>
2020-12-03 05:38:41 +0200

    Setup the useState and useEffet and the validation

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,77 @@ import './App.css';
 type State = {
   username: string
   password:  string
-  
+  usernameError: boolean
+  passwordError: boolean
+  isButtonDisabled: boolean
+  helperText : String
 };
 
+
 const App = () => {
-  const [state , setState] = useState({
-    email : "",
-    password : ""
-})
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [helperText, setHelperText] = useState('');
+
+  const validateUsername = (target:any) => {
+    if (target.value.length >= 6) {
+      setUsername(target.value);
+      setUsernameError(false);
+      setHelperText('');
+    } else {
+      setUsernameError(true);
+      setHelperText('The Username is less than 6 Character');
+    }
+  }
+
+  const validatePassword = (target:any) => {
+    if (target.value.length >= 6) {
+      setPassword(target.value);
+      setPasswordError(false);
+      setHelperText('');
+    } else {
+      setPasswordError(true);
+      setHelperText('Invalid Password. Please try again.');
+    }
+  }
+
+  const handleChange:React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name } = e.target;
+
+    switch (name) {
+      case 'username':
+        validateUsername(e.target);
+        break;
+      case 'password':
+        validatePassword(e.target);
+        break;
+      default:
+        break;
+    }
+  }
+  /**const handleSubmit:React.MouseEvent<HTMLAnchorElement, MouseEvent> = (e) => {
+    
+
+    e.preventDefault();
+  }
+  */
   return (
     <div className="row justify-content-center">
           <div className="col-lg-4 col-md-6 col-sm-8">
             <div className="card">
               <div className="card-body">
-                <h3 className="card-title text-center">LOG APP</h3>
+                <h3 className="card-title text-center">LOGIN APP</h3>
                 <form >
                   
                   <div className="form-group text-left">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" 
+                    <label htmlFor="exampleInputEmail1">Username</label>
+                    <input type="username" 
                             className="form-control" 
-                            id="email" 
-                            placeholder="Enter email" 
+                            id="username" 
+                            placeholder="Enter Username" 
                     />
                     
                   </div>
@@ -35,7 +84,7 @@ const App = () => {
                   <input type="password" 
                           className="form-control" 
                           id="password" 
-                          placeholder="Password"
+                          placeholder="Enter Password"
                           
                   />
                 </div>
@@ -44,15 +93,18 @@ const App = () => {
                 <button 
                       type="submit" 
                       className="btn btn-primary"
-                      
-                >OK</button>
+                      /**onClick={handleSubmit}
+                      disabled={state.isButtonDisabled}
+                      */
+                >LOGIN</button>
                 <small className="form-text text-muted">Wrong Message</small>
                 <hr/>
                 <button 
                       type="submit" 
                       className="btn btn-primary"
+
                       
-                >Clear</button>
+                >CLEAR</button>
                   
                 </form>
               </div>
@@ -61,7 +113,7 @@ const App = () => {
               </div>
             </div>
             <div className="text-center py-2">
-              
+               <p>My name is Patickk and My Password is 252525</p>
             </div>
           </div>
     </div>
